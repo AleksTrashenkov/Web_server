47d50c0fb6b7a1bfaeb03500bd9c9bb74d9a9857
trashenkov_aleks <trashenkov-aleks@mail.ru>
2023-10-14 15:58:36 +0400

    Добавил новые функции на страницу: 1. Прокрутка вверх по стрелке 2. Погода по городам

--- src/main/java/ru/web_server_home/RenameServlet.java
+++ src/main/java/ru/web_server_home/RenameServlet.java
@@ -7,6 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import javax.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
 
 public class RenameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -31,3 +39,43 @@ public class RenameServlet extends HttpServlet {
         }
     }
     }
+
+/*@WebServlet(name = "SystemInfoServlet", urlPatterns = {"/system-info"})
+public class SystemInfoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
+        double cpuLoad = osBean.getSystemCpuLoad() * 100;
+
+        long totalMemory = osBean.getTotalPhysicalMemorySize();
+        long freeMemory = osBean.getFreePhysicalMemorySize();
+
+        request.setAttribute("cpuLoad", cpuLoad);
+        request.setAttribute("totalMemory", totalMemory);
+        request.setAttribute("freeMemory", freeMemory);
+
+        // Передаем управление JSP странице
+        request.getRequestDispatcher("/system-info.jsp").forward(request, response);
+    }
+}*/
+//вывод инфы:
+
+/*
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Состояние ПК</title>
+</head>
+<body>
+<h1>Информация о состоянии ПК</h1>
+<p>CPU Load: <%= request.getAttribute("cpuLoad") %> %</p>
+<p>Total Memory: <%= (Long) request.getAttribute("totalMemory") / (1024 * 1024) %> MB</p>
+<p>Free Memory: <%= (Long) request.getAttribute("freeMemory") / (1024 * 1024) %> MB</p>
+</body>
+</html>*/
